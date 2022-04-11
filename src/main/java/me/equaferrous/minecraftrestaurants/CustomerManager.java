b67feea5c2a63c22980ab71071519c30a4861301
benbrooks15 <102412238+EquaFerrous@users.noreply.github.com>
2022-04-11 13:51:26 +0100

    Random Customer Tiers

--- src/main/java/me/equaferrous/minecraftrestaurants/CustomerManager.java
+++ src/main/java/me/equaferrous/minecraftrestaurants/CustomerManager.java
@@ -1,15 +1,20 @@
 package me.equaferrous.minecraftrestaurants;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class CustomerManager {
 
     private static CustomerManager instance;
 
     private List<Customer> customerList = new ArrayList<>();
+    private HashMap<Integer, Double> customerTierWeights = new HashMap<>();
+    private Random random = new Random();
 
     // --------------------------------------------------------------
 
@@ -17,6 +22,8 @@ public class CustomerManager {
         if (instance == null) {
             instance = this;
         }
+
+        SetupTierWeights();
     }
 
     // ----------------------------------------------------------------
@@ -28,7 +35,38 @@ public class CustomerManager {
     // ---------------------------------------------------------------
 
     public void SpawnCustomer(Location location) {
-        Customer newCustomer = new Customer(location, 1);
+        Customer newCustomer = new Customer(location, GetRandomCustomerTier());
         customerList.add(newCustomer);
+    }
+
+    // -----------------------------------------------------------------
+
+    // Sets up the dictionary of tier weights
+    private void SetupTierWeights() {
+        customerTierWeights.putIfAbsent(1, 40.0);
+        customerTierWeights.putIfAbsent(2, 35.0);
+        customerTierWeights.putIfAbsent(3, 25.0);
+    }
+
+    // Chooses a random customer tier
+    private int GetRandomCustomerTier() {
+        double totalWeight = 0;
+        for (Double tierWeight : customerTierWeights.values()) {
+            totalWeight += tierWeight;
+        }
+
+        double choice = random.nextDouble() * totalWeight;
+        Bukkit.broadcastMessage(String.valueOf(choice));
+
+        for (Integer tier : customerTierWeights.keySet()) {
+            double weight = customerTierWeights.get(tier);
+            if (choice <= weight) {
+                return tier;
+            }
+            else {
+                choice -= weight;
+            }
+        }
+        return 0;
     }
 }
